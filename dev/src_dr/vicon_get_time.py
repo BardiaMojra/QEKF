e4e16978af72499221b2a062efdd4433a7238af5
bardi <bardia.mojra@gmail.com>
2022-02-15 18:23:40 -0600

    Updated with git-express on 2022-02-15.

--- dev/src_dr/vicon_get_time.py
+++ dev/src_dr/vicon_get_time.py
@@ -1,21 +1,32 @@
 
 
 import pandas as pd
+import json
 
 from util_dr import *
 from nbug import *
 
 
+
+
 ''' module config
 '''
+VICON__DATA_FNAME = 'vi_clean.csv'
+VICON__DATA_FNAME = 'vicon_test_001.json'
+# imuDir = r"../data/dead_reckoning_data/1/imu/"
+viconDir = "../data/dead_reckoning_data/1/vicon/"
+
+
+with open('path_to_file/person.json') as f:
+  data = json.load(f)
+
+print(data)
 end_epoch=855981016016
 start_epoch=540666478166
-imuDir = r"../data/dead_reckoning_data/1/imu/"
-viconDir = r"../data/dead_reckoning_data/1/vicon/"
 
-fname = 'vi_clean.csv'
+fname =
 vicon_df = pd.read_csv(viconDir+fname, header=0)
-vicon_df.drop(labels=['Frame'], axis=1, inplace=True)
+# vicon_df.drop(labels=['Frame'], axis=1, inplace=True)
 # start_epoch = get_unix_time_from_str(START_TIME)
 # end_epoch = get_unix_time_from_str(END_TIME)
 numSamps = vicon_df.shape[0]
