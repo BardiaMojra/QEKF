4b7f889e829cba27751dab90e65f31f47418462c
bardi <bardia.mojra@gmail.com>
2022-06-22 15:27:01 -0500

    Updated with git-express on 2022-06-22.

--- dev/XEst/main.m
+++ dev/XEst/main.m
@@ -1,19 +1,20 @@
-%% XEst main 
+%% XEst main
+% @link https://www.mathworks.com/help/vision/ug/monocular-visual-simultaneous-localization-and-mapping.html
 
 %% init
-close all; clear; clc;  
+close all; clear; clc;
 cfg   = config_class(TID  = 'T00001_0-40fr_150surf', benchmark  = 'KITTI');
 pfeat = pfeature_class(); pfeat.load_cfg(cfg);
-dlog  = dlogger_class(); dlog.load_cfg(cfg); 
-quest = quest_class(); quest.load_cfg(cfg); 
-vest  = vest_class(); vest.load_cfg(cfg); 
-qekf  = qekf_handler_class(); qekf.load_cfg(cfg); 
+dlog  = dlogger_class(); dlog.load_cfg(cfg);
+quest = quest_class(); quest.load_cfg(cfg);
+vest  = vest_class(); vest.load_cfg(cfg);
+qekf  = qekf_handler_class(); qekf.load_cfg(cfg);
 rpt   = report_class(); rpt.load_cfg(cfg);
 %% todo: keep track features for x number of frames
 
-%% run 
-cntr  = 0;  
-for kf = cfg.kframes % --->> iter keyframes 
+%% run
+cntr  = 0;
+for kf = cfg.kframes % --->> iter keyframes
   cntr = cntr+1;
   pfeat.get_pmats(kf, cfg.dat);
   TQVW_sols = quest.get_pose(kf, cfg.dat); % get pose
@@ -21,7 +22,7 @@ for kf = cfg.kframes % --->> iter keyframes
   st_sols   = qekf.run_filter(TQVW_sols); % run filter
 
   dlog.log_state(cntr, kf, TQVW_sols, st_sols);
-end 
+end
 
 %% results
 dlog.get_logs();
